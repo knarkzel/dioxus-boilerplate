7f8e9a05c459bbb81839c124764de99b437bbeb0
Knarkzel <knarkzel@gmail.com>
2023-02-11 19:37:11 +0100

    Improve boilerplate

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,23 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
-    cx.render(rsx!{
-        h1 { "Hello world!" }
+    cx.render(rsx! {
+        main {
+            class: "container",
+            h1 {
+                class: "title is-1 my-4 has-text-centered",
+                "Hello world!",
+            },
+            div {
+                class: "content",
+                ul {
+                    (1..=10).map(|i| rsx! {
+                        li {
+                            "Item {i}",
+                        },
+                    }),
+                },                
+            },
+        }
     })
 }
